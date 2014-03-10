b7653182058629ae74168ca06877dc631d1e6432
Rory O’Kane <rory@roryokane.com>
2014-03-10 16:07:22 -0400

    Implement live calculation, stub menus, and show sample calculations
    Live calculation is done in the UI thread for now, causing noticeable slowdown.
    
    Menu items have accelerators for OS X, but no mnemonics.
    
    The sample calculations are hardcoded.

--- src/main/java/DocumentWindow.java
+++ src/main/java/DocumentWindow.java
@@ -1,20 +1,38 @@
 import com.google.common.base.Joiner;
+import frink.errors.FrinkEvaluationException;
+import frink.errors.FrinkException;
+import frink.parser.Frink;
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import java.util.*;
 
 public class DocumentWindow {
     public JPanel root;
     private JTextArea calculationsTextArea;
     private JTextArea lineNumbersTextArea;
-    String calculationsText = "";
+    private JTextArea resultsTextArea;
+    private JScrollPane lineNumbersScrollArea;
+    private JScrollPane calculationsScrollPane;
+    private JScrollPane resultsScrollPane;
 
     public DocumentWindow() {
-        populateLineNumbers();
+        calculationsTextArea.getDocument().addDocumentListener(new CalculationDocumentChangeListener());
+        prefillSampleCalculations();
+        updateResultsAndLineNumbers();
+    }
+
+    private void prefillSampleCalculations() {
+        calculationsTextArea.setText("height = 5 feet + 9 inches -> \"feet\"\npeople_in_room = 53\nsize_of_stack = height * people_in_room");
+    }
+
+    public void focusCalculationsArea() {
+        calculationsTextArea.grabFocus();
     }
 
     private void populateLineNumbers() {
-        int numLines = calculationsLines().size();
+        int numLines = getCalculationsLines().size();
 
         List<String> lineLabels = new ArrayList<String>();
         for (int i = 1; i <= numLines; i++) {
@@ -25,13 +43,54 @@ public class DocumentWindow {
         lineNumbersTextArea.setText(lineNumbersText);
     }
 
-    private List<String> calculationsLines() {
-        String[] lines = calculationsText.split("\r\n|\r|\n");
+    private List<String> getCalculationsLines() {
+        String[] lines = getCalculationsText().split("\r\n|\r|\n");
         return Arrays.asList(lines);
     }
 
-    private JTextArea resultsTextArea;
-    private JScrollPane lineNumbersScrollArea;
-    private JScrollPane calculationsScrollPane;
-    private JScrollPane resultsScrollPane;
+    private String getCalculationsText() {
+        return calculationsTextArea.getText();
+    }
+
+    private class CalculationDocumentChangeListener implements DocumentListener {
+        @Override
+        public void insertUpdate(DocumentEvent documentEvent) {
+            updateResultsAndLineNumbers();
+        }
+
+        @Override
+        public void removeUpdate(DocumentEvent documentEvent) {
+            updateResultsAndLineNumbers();
+        }
+
+        @Override
+        public void changedUpdate(DocumentEvent documentEvent) {
+            updateResultsAndLineNumbers();
+        }
+    }
+
+    private void updateResultsAndLineNumbers() {
+        // TODO do in a separate thread. Register an event to be either run immediately or right after the current event (overwriting other pending similar events).
+        populateLineNumbers();
+        recalculateAllResults();
+    }
+
+    private void recalculateAllResults() {
+        final List<String> calculationsLines = getCalculationsLines();
+        List<String> resultLines = new ArrayList<String>();
+        Frink interpreter = new Frink();
+
+        for (String calculationsLine : calculationsLines) {
+            String result;
+            try {
+                result = interpreter.parseString(calculationsLine);
+            } catch (FrinkException e) {
+                result = e.toString();
+            }
+            resultLines.add(result);
+        }
+
+        String resultsText = Joiner.on("\n").join(resultLines);
+        resultsTextArea.setText(resultsText);
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,19 @@ import frink.parser.Frink;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 
 public class Main {
     public static final Dimension DEFAULT_WINDOW_SIZE = new Dimension(500, 600);
 
     public static void main(String[] args) {
+        System.setProperty("apple.laf.useScreenMenuBar", "true");
+        // TODO set up application bundle: http://stackoverflow.com/q/8955638/578288
+
         JFrame frame = new JFrame("CalcuDoc");
-        frame.setContentPane(new DocumentWindow().root);
+        final DocumentWindow documentWindow = new DocumentWindow();
+        frame.setContentPane(documentWindow.root);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         frame.setJMenuBar(createMenu());
@@ -17,22 +23,61 @@ public class Main {
         frame.pack();
         frame.setSize(DEFAULT_WINDOW_SIZE);
 
+        documentWindow.focusCalculationsArea();
+
         frame.setVisible(true);
     }
 
     public static JMenuBar createMenu() {
+        JMenuItem item;
+
         JMenuBar menuBar = new JMenuBar();
 
         JMenu fileMenu = new JMenu("File");
+        item = new JMenuItem("Open…");
+        item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.META_MASK));
+        fileMenu.add(item);
+        JMenu openRecent = new JMenu("Open Recent"); // TODO save for later and update live
+        fileMenu.add(openRecent);
+        fileMenu.addSeparator();
+        item = new JMenuItem("Save…");
+        item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.META_MASK));
+        fileMenu.add(item);
+        item = new JMenuItem("Save As…");
+        item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.META_MASK + ActionEvent.SHIFT_MASK));
+        fileMenu.add(item);
+        fileMenu.setMnemonic(KeyEvent.VK_F);
         menuBar.add(fileMenu);
 
         JMenu editMenu = new JMenu("Edit");
+        item = new JMenuItem("Cut");
+        item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, ActionEvent.META_MASK));
+        editMenu.add(item);
+        item = new JMenuItem("Copy");
+        item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.META_MASK));
+        editMenu.add(item);
+        item = new JMenuItem("Paste");
+        item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V, ActionEvent.META_MASK));
+        editMenu.add(item);
+        editMenu.setMnemonic(KeyEvent.VK_E);
         menuBar.add(editMenu);
 
         JMenu viewMenu = new JMenu("View");
+        item = new JMenuItem("Decrease Text Size");
+        item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_MINUS, ActionEvent.META_MASK));
+        viewMenu.add(item);
+        item = new JMenuItem("Increase Text Size");
+        item.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_EQUALS, ActionEvent.META_MASK));
+        viewMenu.add(item);
+        viewMenu.setMnemonic(KeyEvent.VK_V);
         menuBar.add(viewMenu);
 
         JMenu helpMenu = new JMenu("Help");
+        item = new JMenuItem("Open Example Document");
+        helpMenu.add(item);
+        item = new JMenuItem("Open Frink Language Documentation");
+        helpMenu.add(item);
+        helpMenu.setMnemonic(KeyEvent.VK_H);
         menuBar.add(helpMenu);
 
         return menuBar;
